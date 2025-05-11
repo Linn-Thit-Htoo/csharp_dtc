5564cb46f9836ebce226ddb1fd59676ef0d0cf0d
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-12 00:02:57 +0630

    -

--- csharp_dtc.API/Models/OrderDetailRequestModel.cs
+++ csharp_dtc.API/Models/OrderDetailRequestModel.cs
@@ -1,11 +1,10 @@
-﻿namespace csharp_dtc.API.Models
+﻿namespace csharp_dtc.API.Models;
+
+public class OrderDetailRequestModel
 {
-    public class OrderDetailRequestModel
-    {
-        public string ProductName { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
-        public decimal SubTotal { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public decimal SubTotal { get; set; }
+    public DateTime CreatedAt { get; set; }
 }

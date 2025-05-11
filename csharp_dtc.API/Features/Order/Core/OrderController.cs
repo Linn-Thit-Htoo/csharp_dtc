d98a54732326fd27693884bbdea58bda90b1b17c
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-11 22:40:09 +0630

    -

--- csharp_dtc.API/Features/Order/Core/OrderController.cs
+++ csharp_dtc.API/Features/Order/Core/OrderController.cs
@@ -4,24 +4,23 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace csharp_dtc.API.Features.Order.Core
+namespace csharp_dtc.API.Features.Order.Core;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OrderController : BaseController
+    private readonly ISender _sender;
+
+    public OrderController(ISender sender)
     {
-        private readonly ISender _sender;
+        _sender = sender;
+    }
 
-        public OrderController(ISender sender)
-        {
-            _sender = sender;
-        }
-
-        [HttpPost("SubmitOrder")]
-        public async Task<IActionResult> SubmitOrder(SubmitOrderRequest request, CancellationToken cs)
-        {
-            var result = await _sender.Send(request, cs);
-            return Content(result);
-        }
+    [HttpPost("SubmitOrder")]
+    public async Task<IActionResult> SubmitOrder(SubmitOrderRequest request, CancellationToken cs)
+    {
+        var result = await _sender.Send(request, cs);
+        return Content(result);
     }
 }

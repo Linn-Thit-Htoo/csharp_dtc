f4867ff0b28be716da994bbab843f54834c5ee5c
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-11 22:39:36 +0630

    -

--- csharp_dtc.API/Extensions/DependencyInjectionExtensions.cs
+++ csharp_dtc.API/Extensions/DependencyInjectionExtensions.cs
@@ -4,49 +4,48 @@ using csharp_dtc.API.OrderDetailDbContextModels;
 using Microsoft.EntityFrameworkCore;
 using System.Transactions;
 
-namespace csharp_dtc.API.Extensions
+namespace csharp_dtc.API.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        TransactionManager.ImplicitDistributedTransactions = true;
+
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+            opt.JsonSerializerOptions.DictionaryKeyPolicy = null;
+        });
 
-            TransactionManager.ImplicitDistributedTransactions = true;
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+        });
 
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-                opt.JsonSerializerOptions.DictionaryKeyPolicy = null;
-            });
+        builder.Services.AddDbContext<OrderDbContext>(opt =>
+        {
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("Dtc1DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddMediatR(config =>
-            {
-                config.RegisterServicesFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
-            });
+        builder.Services.AddDbContext<OrderDetailDbContext>(opt =>
+        {
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("Dtc2DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-            builder.Services.AddDbContext<OrderDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("Dtc1DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            });
+        builder.Services.AddScoped<csharp_dtc.API.OrderPersistence.Wrapper.IUnitOfWork, csharp_dtc.API.OrderPersistence.Wrapper.UnitOfWork>();
+        builder.Services.AddScoped<csharp_dtc.API.OrderDetailPersistence.Wrapper.IUnitOfWork, csharp_dtc.API.OrderDetailPersistence.Wrapper.UnitOfWork>();
+        builder.Services.AddHealthChecks();
+        builder.Services.Configure<AppSetting>(builder.Configuration);
 
-            builder.Services.AddDbContext<OrderDetailDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("Dtc2DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            });
-
-            builder.Services.AddScoped<csharp_dtc.API.OrderPersistence.Wrapper.IUnitOfWork, csharp_dtc.API.OrderPersistence.Wrapper.UnitOfWork>();
-            builder.Services.AddScoped<csharp_dtc.API.OrderDetailPersistence.Wrapper.IUnitOfWork, csharp_dtc.API.OrderDetailPersistence.Wrapper.UnitOfWork>();
-            builder.Services.AddHealthChecks();
-            builder.Services.Configure<AppSetting>(builder.Configuration);
-
-            return services;
-        }
+        return services;
     }
 }

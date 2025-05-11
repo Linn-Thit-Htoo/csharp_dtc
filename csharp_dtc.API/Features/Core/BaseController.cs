ec8ede64bee81fe4ba7d115801506f4969b35c18
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-11 22:39:47 +0630

    -

--- csharp_dtc.API/Features/Core/BaseController.cs
+++ csharp_dtc.API/Features/Core/BaseController.cs
@@ -2,15 +2,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace csharp_dtc.API.Features.Core
+namespace csharp_dtc.API.Features.Core;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(JsonConvert.SerializeObject(obj), "application/json");
-        }
+        return Content(JsonConvert.SerializeObject(obj), "application/json");
     }
 }

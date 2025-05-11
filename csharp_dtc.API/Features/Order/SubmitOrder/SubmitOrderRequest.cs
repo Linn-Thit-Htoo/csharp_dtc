fa3ba0664cf7f46decea46ffbe636942d6bc2b12
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-11 22:40:45 +0630

    -

--- csharp_dtc.API/Features/Order/SubmitOrder/SubmitOrderRequest.cs
+++ csharp_dtc.API/Features/Order/SubmitOrder/SubmitOrderRequest.cs
@@ -2,11 +2,10 @@
 using csharp_dtc.API.Utils;
 using MediatR;
 
-namespace csharp_dtc.API.Features.Order.SubmitOrder
+namespace csharp_dtc.API.Features.Order.SubmitOrder;
+
+public class SubmitOrderRequest : IRequest<BaseResponse<SubmitOrderResponse>>
 {
-    public class SubmitOrderRequest : IRequest<BaseResponse<SubmitOrderResponse>>
-    {
-        public decimal GrandTotal { get; set; }
-        public List<OrderDetailRequestModel> OrderDetail { get; set; }
-    }
+    public decimal GrandTotal { get; set; }
+    public List<OrderDetailRequestModel> OrderDetail { get; set; }
 }

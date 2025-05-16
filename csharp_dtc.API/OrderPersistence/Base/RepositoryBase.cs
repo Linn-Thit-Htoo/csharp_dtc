1b552d35d9589ee8b9b97603d33f04950acfe735
Lin Thit Htoo <lth1212001@gmail.com>
2025-05-17 00:17:28 +0630

    -

--- csharp_dtc.API/OrderPersistence/Base/RepositoryBase.cs
+++ csharp_dtc.API/OrderPersistence/Base/RepositoryBase.cs
@@ -1,5 +1,4 @@
-﻿using csharp_dtc.API.OrderDetailPersistence.Base;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 
